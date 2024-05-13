c9874cb179a7670c8ab210a956ba7451d2704a74
MRFAutomation <mrf.testdevice@gmail.com>
2024-05-13 14:26:05 +0500

    Second JS Assignment

---
+++ day2tasks.js
@@ -0,0 +1,29 @@
+/*
+Task#1:
+When a variable is declared with the let keyword, you can reassign (or change the value of) that variable later on. In this example, the value of testername is changed
+from "Asad" to "Khalid".
+ 
+let testername = "Asad";
+testername = "Khalid";
+
+Note that when reassigning a variable that has already been declared, you do not use the let keyword.
+You need to assign anyone's name in "testname" and then reassign variable with your name and print it using console.log() function.
+*/
+
+/* Solution */
+
+let testername = "Rumman";
+testername = "Farhan";
+console.log("Tester name is: " + testername);
+
+/*
+Task#2:
+In JavaScript, when variable name is have more than one word like "firstvariable" then there is a specific naming convention to use is camel case. Like
+let variableOne;
+let secondVariable;
+In Task#1, update the variable name and use camel case naming convention.
+*/
+/* Solution */
+let cqeName = "Rumman";
+cqeName = "Farhan";
+console.log("CQE name is: " + cqeName);
