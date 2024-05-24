ad1fcf81f7f8a6464a8e2e25e1c1c495a83976cf
MRFAutomation <mrf.testdevice@gmail.com>
2024-05-24 18:01:28 +0500

    Day 9 JS Assignment print middle value

--- day9task.js
+++ day9task.js
@@ -1,3 +1,21 @@
+/*
+You need to wright a program in which you have to print the middle element of an array using "length" function.
+Use console.log() function to print the element.
+ 
+Example array:
+let arr = ["Farrukh", "Asad", "Naveed", "Farhan", "Osama"]
+ 
+In above case, your program should print "Naveed"
+*/
+
+
 let arr = ["Farrukh", "Asad", "Naveed", "Farhan", "Osama"];
-let mid = (arr.length - 1) / 2;
+
+let arrLength = arr.length;
+let mid = 0;
+
+if (arrLength % 2 == 1) {
+    mid = (arrLength - 1) / 2;
+}
+
 console.log(arr[mid]);
