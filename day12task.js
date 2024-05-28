8e54f582e1d1177d4bfc62eb324f848b521c47d0
MRFAutomation <mrf.testdevice@gmail.com>
2024-05-28 12:34:15 +0500

    Day 12 JS Assignment Return Even Odd based on Array length

---
+++ day12task.js
@@ -0,0 +1,40 @@
+/*
+You need to write a function checkOdd which returns the true if the input array is odd and false otherwise.
+ 
+Example array:
+let arr = ["Farrukh", "Asad", "Naveed", "Farhan", "Osama"]
+ 
+function checkOdd(<pass array here>){
+ 
+}
+*/
+/* Solution */
+let arr1 = ["Farrukh", "Asad", "Naveed", "Farhan", "Osama"]
+
+function checkOdd(arr) {
+    if (arr.length % 2 == 1) {
+        return true;
+    }
+}
+if (checkOdd(arr1)) {
+    console.log("Array is Odd");
+}
+
+/*
+Write a function to print an array in reverse order. For example if I provide following as an input
+let arr = ["Farrukh", "Asad", "Naveed", "Farhan", "Osama"]
+ 
+Your program should return
+[ "Osama", "Farhan", "Naveed", "Asad", "Farrukh"]
+*/
+/* Solution */
+let arr2 = ["Farrukh", "Asad", "Naveed", "Farhan", "Osama"]
+
+function checkEven(arr) {
+    if (arr.length % 2 == 0) {
+        return true;
+    }
+}
+if (checkOdd(arr2)) {
+    console.log("Array is Even");
+}
